2e77b3e18fd632310864358588930c50ffe9be1d
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2025-06-30 20:17:31 -0400

    clean up

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,11 +4,7 @@
 
 
 int main(int argc, char *argv[]) {
-    //Checking if there are no command line arguments  (valid but will not be calling the parse function)
-    if(argc == 1){ //there are no arguments passed 
-        std::cout << "No command line arguments passed" << std::endl; 
-    } else{ // There are arguments passed call to parsing function
-        std::cout << "Call to the parse function to get arguments" << std::endl;
+    if(argc != 1){ //there are arguments passed 
         std::vector<std::string> arguments_passed(argv + 1, argv + argc);
         Parser arguments = Parse_arguments(arguments_passed);
     }
